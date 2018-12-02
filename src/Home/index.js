cccb9cd17b2a2e2afbbe8852be65cd53d58742a1
ayy oulidi <ayyouboulidiomali@gmail.com>
2018-12-02 13:50:59 +0100

    fix(Tests): fix unit testing

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import './Home.css';
 
 class App extends Component {
+  goTo = (link) => this.props.history.push(link)
+
   render() {
     return (
       <div className="home">
           Please hit a menu element to view the different pages <br />
-          - <Link to='/'>Home</Link>  <br />
-          - <Link to='/actors'>Actors</Link>  : the list of starwars actors <br />
-          - <Link to='/cars'>Cars</Link>  : the list of starwars cars <br />
-          - to view the details, please click on the wanted element <br />
+          - <span className="link" onClick={() => this.goTo('/')}>Home</span>  <br />
+          - <span className="link" onClick={() => this.goTo('/actors')}>Actors</span>  : the list of starwars actors <br />
+          - <span className="link" onClick={() => this.goTo('/cars')}>Cars</span>  : the list of starwars cars <br />
+          - to view the details, please click on the wanted element <br /><br />
+          To launch the project: yarn start <br />
+          To launch minimal unit tests : yarn test
       </div>
     );
   }
--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import Header from './Header';
 
@@ -7,7 +7,19 @@ export default function RouterApp() {
   return (
     <Router>
         <div>
-            <Header />   
+            <Header>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/actors'>Actors</Link>
+                    </li>
+                    <li>
+                        <Link to='/cars'>Cars</Link>
+                    </li>
+                </ul>
+            </Header>   
             <Route exact path='/' component={Home} />
             <Route path='/actors' component={Home} />
             <Route path='/actor/:id' component={Home} />
